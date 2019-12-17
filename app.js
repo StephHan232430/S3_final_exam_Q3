6e68cc5e254ad77f54a44ed748a5a8b3571d7906
Steph <blackman914037@gmail.com>
2019-12-17 14:27:55 +0800

    add user routers

--- app.js
+++ app.js
@@ -43,35 +43,7 @@ app.get('/', (req, res) => {
   res.send('homepage')
 })
 
-// 認證系統路由
-// 註冊頁面
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-
-// 註冊檢查
-app.post('/users/register', (req, res) => {
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(user => res.redirect('/'))
-})
-
-// 登入頁面
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-
-// 登入檢查
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
-
-// 登出
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+app.use('/users', require('./routes/user'))
 
 app.listen(port, () => {
   console.log(`App is running on ${port}`)
