644b4f34ae777318fbaf0a9749cf7cf8f7df77e0
Steph <blackman914037@gmail.com>
2019-12-17 11:26:52 +0800

    Initialize project

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+const port = 3000
+
+const flash = require('connect-flash')
+const session = require('express-session')
+const passport = require('passport')
+
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'main' }))
+app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.send('homepage')
+})
+
+app.listen(port, () => {
+  console.log(`App is running on ${port}`)
+})
